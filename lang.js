5aaa9648bb37f4732802ea1c1b028b6da939194f
vlad12e <humanaccaunt@gmail.com>
2024-09-26 22:18:33 +0300

    DZ 5

---
+++ lang.js
@@ -0,0 +1,32 @@
+const langArr = {
+    title: {
+        ua: 'Мульти мовна сторінка',
+        en: 'Multilingual  page',
+        ge: 'Mehrsprachige Seite'
+    },
+    name: {
+        ua: 'Ноутбук Lenovo',
+        en: 'LapTop Lenovo',
+        ge: 'Lenovo-Laptop'
+    },
+    os: {
+        ua: 'Операційна система',
+        en: 'Operation system',
+        ge: 'Betriebssystem'
+    },
+    memory: {
+        ua: "Об'єм пам'яті",
+        en: 'Memory capacity',
+        ge: 'Die Menge an Speicher'
+    },
+    type: {
+        ua: "Тип пам'яті",
+        en: "Memory type",
+        ge: 'Speichertyp'
+    },
+    color: {
+        ua: "Колір",
+        en: "Color",
+        ge: 'Farbe'
+    },
+}
